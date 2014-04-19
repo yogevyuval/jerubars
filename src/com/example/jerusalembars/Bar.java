f85acf9d7578dfd9848c1cc846bbdf7e0ffb4845
Michael Lantsman <michlan@me.com>
2014-04-19 20:06:56 +0300

    bar

---
+++ src/com/example/jerusalembars/Bar.java
@@ -0,0 +1,13 @@
+package com.example.jerusalembars;
+
+import java.util.HashMap;
+
+public class Bar {
+	private String name;
+	private String happyHour;
+//	private Loaction loc;
+	public Bar(String name, String happyHour){
+		this.name = name;
+		this.happyHour = happyHour;
+	}
+}
